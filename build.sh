74a26d050dc71e6f8661b840dd9d28f8c664caf1
KryptonLee <jun.k.lee199410@outlook.com>
2022-09-07 14:51:51 +0800

    Update to CoreELEC 19.5-Matrix_rc2 with official support for uwe5621ds

--- build.sh
+++ build.sh
@@ -1,84 +1,39 @@
 #! /bin/sh
-version="19.4-Matrix"
+version="19.5-Matrix_rc2"
 script_root="${PWD}"
-coreelec_name="CoreELEC-${version}"
-coreelec_code_path="${script_root}/${coreelec_name}"
-coreelec_source_file="${script_root}/${coreelec_name}.tar.gz"
-coreelec_source_url="https://github.com/CoreELEC/CoreELEC/archive/refs/tags/${version}.tar.gz"
-amlogic_driver_path="${coreelec_code_path}/projects/Amlogic-ce/packages/linux-drivers/amlogic"
-patch_file="${script_root}/patches/19.4/0001-fix-compile-error.patch"
-output_dir="${script_root}/output"
-image_prefix="CoreELEC-Amlogic-ng.arm-19.4-Matrix"
-image_name="${image_prefix}-E900V22C-$(date +%Y.%m.%d)"
+source_img_name="CoreELEC-Amlogic-ng.arm-${version}-Generic"
+source_img_file="${script_root}/${source_img_name}.img.gz"
+source_img_url="https://github.com/CoreELEC/CoreELEC/releases/download/${version}/${source_img_name}.img.gz"
+target_img_prefix="CoreELEC-Amlogic-ng.arm-${version}"
+target_img_name="${target_img_prefix}-E900V22C-$(date +%Y.%m.%d)"
 mount_point="${script_root}/mnt"
 dtb_file="${script_root}/common-files/e900v22c.dtb"
 cache_cfg="${script_root}/common-files/advancedsettings.xml"
 rc_maps_cfg="${script_root}/common-files/rc_maps.cfg"
 rc_keymap_file="${script_root}/common-files/e900v22c.rc_keymap"
 remap_backspace_file="${script_root}/common-files/backspace.xml"
+wifi_module_load_file="${script_root}/common-files/wifi_dummy.conf"
+bt_service_file="${script_root}/common-files/sprd_sdio-firmware-aml.service"
 
-add_driver_package() {
-	echo "Copying uwe5621ds wifi driver source code"
-	cp -rf ./UWE5621DS-aml ${amlogic_driver_path}
-}
-
-apply_patches() {
-	echo "Applying patches"
-	git apply --directory=${coreelec_name} ${patch_file}
-}
-
-download_source_code() {
-	echo "Start downloading CoreELEC-${version} source code"
-	wget ${coreelec_source_url} -O ${coreelec_source_file}
-	tar -zxf ${coreelec_source_file}
-	rm -f ${coreelec_source_file}
-}
-
-prepare_source_code() {
-	if [ -d "${coreelec_code_path}" ]; then
-		echo "Using CoreELEC-${version} source code in local directory"
-	else
-		download_source_code
-		add_driver_package
-		apply_patches
-	fi
-}
-
-build_coreelec() {
-	echo "Starting building CoreELEC"
-	cd ${coreelec_code_path}
-	CUSTOM_GIT_HASH="_"
-	export CUSTOM_GIT_HASH
-	make image || exit 1
-}
-
-move_img2out_dir() {
-	if [ ! -d "${script_root}/output" ]; then
-		echo "Creating output directory"
-		mkdir ${output_dir}
-	fi
-	echo "Moving CoreELEC image to output directory"
-	mv ${coreelec_code_path}/target/${image_prefix}*-Generic.img.gz \
-		${output_dir}/${image_name}.img.gz
-}
-
-delete_unneeded_img() {
-	echo "Deleting unneeded CoreELEC images"
-	rm -f ${coreelec_code_path}/target/*
+download_source_img() {
+	echo "Downloading CoreELEC-${version} generic image"
+	wget ${source_img_url} -O ${source_img_file} | exit 1
 }
 
 decompress_img() {
 	echo "Decompressing CoreELEC image"
-	gzip -d ${output_dir}/${image_name}.img.gz
+	gzip -d ${source_img_file}
 }
 
 add_dtb2img() {
 	echo "Creating mount point"
 	mkdir ${mount_point}
 	echo "Mounting CoreELEC boot partition"
-	sudo mount -o loop,offset=4194304 ${output_dir}/${image_name}.img ${mount_point}
-	echo "Copying DTB file into boot partition"
+	sudo mount -o loop,offset=4194304 ${script_root}/${source_img_name}.img ${mount_point}
+
+	echo "Copying E900V22C DTB file"
 	sudo cp ${dtb_file} ${mount_point}/dtb.img
+
 	echo "Unmounting CoreELEC boot partition"
 	sudo umount -d ${mount_point}
 	echo "Deleting mount point"
@@ -89,40 +44,46 @@ add_custom_cfg2img() {
 	echo "Creating mount point"
 	mkdir ${mount_point}
 	echo "Mounting CoreELEC data partition"
-	sudo mount -o loop,offset=541065216 ${output_dir}/${image_name}.img ${mount_point}
-	echo "Creating userdata directory in data partition"
+	sudo mount -o loop,offset=541065216 ${script_root}/${source_img_name}.img ${mount_point}
+
+	echo "Creating userdata directory for kodi"
 	sudo mkdir -p -m 0755 ${mount_point}/.kodi/userdata
-	echo "Copying cache config file into data partition"
+	echo "Copying kodi cache config file"
 	sudo cp ${cache_cfg} ${mount_point}/.kodi/userdata/advancedsettings.xml
 	sudo chmod 0644 ${mount_point}/.kodi/userdata/advancedsettings.xml
-	echo "Creating rc_keymaps directory in data partition"
+
+	echo "Creating rc_keymaps directories"
 	sudo mkdir -p -m 0775 ${mount_point}/.config/rc_keymaps
 	sudo mkdir -p -m 0755 ${mount_point}/.kodi/userdata/keymaps
-	echo "Copying rc_keymap files into data partition"
+	echo "Copying rc_keymap files"
 	sudo cp ${rc_maps_cfg} ${mount_point}/.config/rc_maps.cfg
 	sudo chmod 0664 ${mount_point}/.config/rc_maps.cfg
 	sudo cp ${rc_keymap_file} ${mount_point}/.config/rc_keymaps/e900v22c
 	sudo chmod 0664 ${mount_point}/.config/rc_keymaps/e900v22c
 	sudo cp ${remap_backspace_file} ${mount_point}/.kodi/userdata/keymaps/backspace.xml
 	sudo chmod 0644 ${mount_point}/.kodi/userdata/keymaps/backspace.xml
+
 	echo "Unmounting CoreELEC boot partition"
 	sudo umount -d ${mount_point}
 	echo "Deleting mount point"
 	rm -rf ${mount_point}
 }
 
+rename_img() {
+	echo "Rename image file"
+	mv ${script_root}/${source_img_name}.img ${script_root}/${target_img_name}.img
+}
+
 compress_img() {
 	echo "Compressing CoreELEC image"
-	gzip ${output_dir}/${image_name}.img
-	sha256sum ${output_dir}/${image_name}.img.gz > ${output_dir}/${image_name}.img.gz.sha256
+	gzip ${script_root}/${target_img_name}.img
+	sha256sum ${script_root}/${target_img_name}.img.gz > ${script_root}/${target_img_name}.img.gz.sha256
 }
 
 echo "Welcome to build CoreELEC for Skyworth E900V22C!"
-prepare_source_code
-build_coreelec
-move_img2out_dir
-delete_unneeded_img
+download_source_img
 decompress_img
 add_dtb2img
 add_custom_cfg2img
+rename_img
 compress_img
